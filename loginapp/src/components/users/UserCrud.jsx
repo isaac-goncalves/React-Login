370fa570ceb98d3c6c060e68ec97e42f13704a94
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-02-16 22:08:51 -0300

    COMMIT

--- loginapp/src/components/users/UserCrud.jsx
+++ loginapp/src/components/users/UserCrud.jsx
@@ -14,7 +14,6 @@ const initialState = {
     user: { name: '', email: '' },
     list: []
 }
-
 export default class UserCrud extends Component {
 
     state = { ...initialState }
@@ -35,13 +34,14 @@ export default class UserCrud extends Component {
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
         axios[method](url, user)
             .then(resp => {
-                const list = this.getUpdateList(resp.data)
+                const list = this.getUpdatedList(resp.data)
                 this.setState({ user: initialState.user, list })
             })
     }
-    getUpdateList(user, add = true) {
+
+    getUpdatedList(user, add = true) {
         const list = this.state.list.filter(u => u.id !== user.id)
-        if (user) list.unshift(user)
+        if(add) list.unshift(user)
         return list
     }
 
@@ -73,13 +73,14 @@ export default class UserCrud extends Component {
                                 name="email"
                                 value={this.state.user.email}
                                 onChange={e => this.updateField(e)}
-                                placeholder="Digite o email..." />
+                                placeholder="Digite o e-mail..." />
                         </div>
                     </div>
                 </div>
+
                 <hr />
                 <div className="row">
-                    <div className="col-12 d-flex justify-content-end" >
+                    <div className="col-12 d-flex justify-content-end">
                         <button className="btn btn-primary"
                             onClick={e => this.save(e)}>
                             Salvar
@@ -98,25 +99,27 @@ export default class UserCrud extends Component {
     load(user) {
         this.setState({ user })
     }
+
     remove(user) {
-        axios.delete(`${baseurl}/${user.id}`).then(resp => {
-            const list = this.getUpdateList(user, false)
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false)
             this.setState({ list })
         })
     }
 
     renderTable() {
         return (
-            <table className="table">
+            <table className="table mt-4">
                 <thead>
                     <tr>
+                        <th>ID</th>
                         <th>Nome</th>
                         <th>E-mail</th>
                         <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.renderRows}
+                    {this.renderRows()}
                 </tbody>
             </table>
         )
@@ -131,25 +134,24 @@ export default class UserCrud extends Component {
                     <td>{user.email}</td>
                     <td>
                         <button className="btn btn-warning"
-                        onClick={() => this.load(user)}>
+                            onClick={() => this.load(user)}>
                             <i className="fa fa-pencil"></i>
                         </button>
                         <button className="btn btn-danger ml-2"
-                        onClick={() => this.remove(user)}>
+                            onClick={() => this.remove(user)}>
                             <i className="fa fa-trash"></i>
                         </button>
-                    
                     </td>
                 </tr>
             )
-        }
-        )
+        })
     }
-
+    
     render() {
         return (
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
